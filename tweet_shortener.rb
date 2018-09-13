cd2e8a9c377ec69fa2529d355440b6d4626faf22
Shuo Wang <gken90@gmail.com>
2018-09-13 21:39:48 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -54,12 +54,5 @@ def shortened_tweet_truncator(string)
     string.truncate(140)
   else
     string
-  end 
   end
 end
-
-
-4. Write another method, `shortened_tweet_truncator`, that truncates the tweet to 140 characters with an ellipsis (...)
-if it's still too long after substitution. E.g. "Random Passage satisfies the craving for those details that ..."
-  * **Hint:** Strings are indexed like arrays. That means you can return string index elements, or a set of string index elements,
-  using the `[]` method. For example:
