3238712cb445c06a63ba97555b1fb18a79afd067
Shuo Wang <gken90@gmail.com>
2018-09-13 20:11:39 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -2,17 +2,19 @@
 
 # word_substituter
 
-def dictionary 
+def dictionary
   {
   "hello" => 'hi'
   "to"  => '2'
   "two"  => '2'
-  "too" => '2' 
+  "too" => '2'
   "for" => '4'
   "four" => '4'
   'be' => 'b'
   'you' => 'u'
-  "at" => "@" 
+  "at" => "@"
   "and" => "&"
 }
-end 
+end
+
+p dictionary
