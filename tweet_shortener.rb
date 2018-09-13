57bf4856bb35a1ea0b97074b36589be5da903b0e
Shuo Wang <gken90@gmail.com>
2018-09-13 20:13:33 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -4,16 +4,16 @@
 
 def dictionary
   {
-  "hello" => 'hi'
-  "to"  => '2'
-  "two"  => '2'
-  "too" => '2'
-  "for" => '4'
-  "four" => '4'
-  'be' => 'b'
-  'you' => 'u'
-  "at" => "@"
-  "and" => "&"
+  "hello" => 'hi',
+  "to"  => '2',
+  "two"  => '2',
+  "too" => '2',
+  "for" => '4',
+  "four" => '4',
+  'be' => 'b',
+  'you' => 'u',
+  "at" => "@",
+  "and" => "&",
 }
 end
 
